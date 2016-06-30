1061161619702c783a977359268f736b600e20e9
Jordan Norlen <noreply@phantomdata.com>
2016-06-29 22:56:15 -0500

    Style tweaks to steam_service

--- app/services/steam_service.rb
+++ app/services/steam_service.rb
@@ -1,7 +1,9 @@
 # An abstraction service for working with the ::SteamApi module within this
 # application.
 class SteamService
-  def self.steam_id_for vanity_name
-    ::SteamApi::steam_id_for vanity_name
+  # Leverages the SteamApi and fetches the steam_id for the given
+  # vanity_name.
+  def self.steam_id_for(vanity_name)
+    ::SteamApi.steam_id_for(vanity_name)
   end
 end
