4d2ab42ffe3a1a9624a427573733c51876fc271e
Jordan Norlen <noreply@phantomdata.com>
2016-07-01 01:38:57 -0500

    Rebranding to steam-stalker so the projects a bit more catchy

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PhantomGames
+module SteamStalker
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += %W(#{config.root}/services)
